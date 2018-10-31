fdf5e15180a54f10d10e3cd7a6faa5d15afc03de
AmazingUU <AmazingUU@163.com>
2018-10-31 21:24:03 +0800

    2018/10/31 21:25

---
+++ spider.py
@@ -0,0 +1,54 @@
+import requests
+
+url = 'https://music.163.com/weapi/v1/resource/comments/R_SO_4_186315?csrf_token='
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
+form_data = {'params':'uT5GVu0knFITNwjjyZw3tbZ7kBbdP/v8FmOBppYgy6TN47glZw4N9AZODi/Ys9DirUsbzHLEai/wQkxoPKTopby+y8yI4i5eyEygJHutO28QTS/NgXdA/TckZlTynMU15FVe1D+d/uEtXV3+hOk9IvYgyiaSPd21FLnUl8xy0Sr60kGWv3bIr+G96ARd7PLU',
+             'encSecKey':'18f6e6a254fb49fb0534c73e857e54d8f50643e5fc8f122794cb5541361e8e65ac4af848d7770af13007735b9e073b05793bd7c3bb8359627403774afa6e2727c613bb8d6a870bba4beae9d9742712b1362d1cb2322f99577fe0138c40627be49cb7ec2860f621c53519d79fdb16a141e23b67539a6b7175603460541fe3b88f'}
+r = requests.post(url,data=form_data,headers = headers)
+print(r.text)
+
+
+var bZH3x=window.asrsea(JSON.stringify(i2x),
+	bwx3x(["流泪","强"]),
+	bwx3x(XD3x.md),
+	bwx3x(["爱心","女孩","惊恐","大笑"]));
+e2x.data=k2x.cJ5O({params:bZH3x.encText,
+	encSecKey:bZH3x.encSecKey})}cQx5C(Z3x,e2x)};v2x.bm3x.redefine=true})();
+
+!function(){
+	function a(a){
+		var d,e,b="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+		c="";
+		for(d=0;a>d;d+=1)
+			e=Math.random()*b.length,
+		e=Math.floor(e),
+		c+=b.charAt(e);
+		return c}
+	function b(a,b){
+		var c=CryptoJS.enc.Utf8.parse(b),
+		d=CryptoJS.enc.Utf8.parse("0102030405060708"),
+		e=CryptoJS.enc.Utf8.parse(a),
+		f=CryptoJS.AES.encrypt(e,c,{iv:d,mode:CryptoJS.mode.CBC});
+		return f.toString()}
+	function c(a,b,c){
+		var d,e;
+		return setMaxDigits(131),
+		d=new RSAKeyPair(b,"",c),
+		e=encryptedString(d,a)}
+	function d(d,e,f,g){
+		var h={},i=a(16);
+		return h.encText=b(d,g),h.encText=b(h.encText,i),h.encSecKey=c(i,e,f),h}
+	function e(a,b,d,e){
+		var f={};
+		return f.encText=c(a+e,b,d),f}
+	window.asrsea=d,window.ecnonasr=e}();
+
+
+
+{rid: "R_SO_4_186315", offset: "0", total: "true", limit: "20", csrf_token: ""}
+
+010001
+
+00e0b509f6259df8642dbc35662901477df22677ec152b5ff68ace615bb7b725152b3ab17a876aea8a5aa76d2e417629ec4ee341f56135fccf695280104e0312ecbda92557c93870114af6c9d05c4f7f0c3685b7a46bee255932575cce10b424d813cfe4875d3e82047b97ddef52741d546b8e289dc6935b3ece0462db0a22b8e7
+
+0CoJUm6Qyw8W8jud
